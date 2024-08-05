6a7eaa66c2272272b07beaf2bebbeae4dfa1e6c0
tserakhau <tserakhau@double.cloud>
2024-08-05 16:13:39 +0300

    TRANSFER-715: Add tross-pr sync

--- transfer_manager/go/pkg/yc/search/disabled_sender.go
+++
@@ -1,8 +0,0 @@
-package search
-
-import "context"
-
-type DisabledSender struct{}
-
-func (s *DisabledSender) Send(_ context.Context, _ *SearchResource) error { return nil }
-func (s *DisabledSender) Close() error                                    { return nil }
