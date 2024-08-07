0dd92306e383c994d83bd8cb9b2759c75b7ed0dc
booec <booec@yandex-team.com>
2024-08-08 00:47:04 +0300

    Add more data for yt2ch test 71ddc1eecf78d02e4fff892ec9714c1c43896a5e

--- transfer_manager/go/tests/helpers/yt_helpers.go
+++ transfer_manager/go/tests/helpers/yt_helpers.go
@@ -13,6 +13,7 @@ import (
 	"github.com/doublecloud/tross/transfer_manager/go/internal/logger"
 	yt2 "github.com/doublecloud/tross/transfer_manager/go/pkg/providers/yt"
 	"github.com/stretchr/testify/require"
+	"go.ytsaurus.tech/yt/go/schema"
 	"go.ytsaurus.tech/yt/go/ypath"
 	"go.ytsaurus.tech/yt/go/yson"
 	"go.ytsaurus.tech/yt/go/yt"
@@ -129,4 +130,135 @@ func YTReadAllRowsFromAllTables[OutRow any](t *testing.T, cluster string, path s
 		resRows = append(resRows, readed...)
 	}
 	return resRows
+}
+
+func YtTypesTestData() ([]schema.Column, []map[string]any) {
+	members := []schema.StructMember{
+		{Name: "fieldInt16", Type: schema.TypeInt16},
+		{Name: "fieldFloat32", Type: schema.TypeFloat32},
+		{Name: "fieldString", Type: schema.TypeString},
+	}
+	elements := []schema.TupleElement{
+		{Type: schema.TypeInt16},
+		{Type: schema.TypeFloat32},
+		{Type: schema.TypeString},
+	}
+
+	listSchema := schema.List{Item: schema.TypeFloat64}
+	structSchema := schema.Struct{Members: members}
+	tupleSchema := schema.Tuple{Elements: elements}
+	namedVariantSchema := schema.Variant{Members: members}
+	unnamedVariantSchema := schema.Variant{Elements: elements}
+	dictSchema := schema.Dict{Key: schema.TypeString, Value: schema.TypeInt64}
+	taggedSchema := schema.Tagged{Tag: "mytag", Item: schema.Tagged{Tag: "innerTag", Item: schema.TypeInt32}}
+
+	schema := []schema.Column{
+		{Name: "id", ComplexType: schema.TypeUint8, SortOrder: schema.SortAscending},
+		{Name: "date_str", ComplexType: schema.TypeBytes},
+		{Name: "datetime_str", ComplexType: schema.TypeBytes},
+		{Name: "datetime_str2", ComplexType: schema.TypeBytes},
+		{Name: "datetime_ts", ComplexType: schema.TypeInt64},
+		{Name: "datetime_ts2", ComplexType: schema.TypeInt64},
+		{Name: "intlist", ComplexType: schema.Optional{Item: schema.TypeAny}},
+		{Name: "num_to_str", ComplexType: schema.TypeInt32},
+		{Name: "decimal_as_float", ComplexType: schema.TypeFloat64},
+		{Name: "decimal_as_string", ComplexType: schema.TypeString},
+		{Name: "decimal_as_bytes", ComplexType: schema.TypeBytes},
+
+		// Composite types below.
+		{Name: "list", ComplexType: listSchema},
+		{Name: "struct", ComplexType: structSchema},
+		{Name: "tuple", ComplexType: tupleSchema},
+		{Name: "variant_named", ComplexType: namedVariantSchema},
+		{Name: "variant_unnamed", ComplexType: unnamedVariantSchema},
+		{Name: "dict", ComplexType: dictSchema},
+		{Name: "tagged", ComplexType: schema.Tagged{Tag: "mytag", Item: schema.Variant{Members: members}}},
+
+		// That test mostly here for CompositeTypesTransformer.
+		// Iteration and transformation over all fields/elements/members of all complex types is tested by it.
+		{Name: "nested1", ComplexType: schema.Struct{Members: []schema.StructMember{
+			{Name: "list", Type: schema.List{
+				Item: schema.Tuple{Elements: []schema.TupleElement{{Type: dictSchema}, {Type: dictSchema}}}},
+			},
+			{Name: "named", Type: schema.Variant{
+				Members: []schema.StructMember{{Name: "d1", Type: dictSchema}, {Name: "d2", Type: dictSchema}},
+			}},
+		}}},
+
+		// Use two different structs to prevent extracting long line to different file from result.json.
+		{Name: "nested2", ComplexType: schema.Struct{Members: []schema.StructMember{
+			{Name: "unnamed", Type: schema.Variant{
+				Elements: []schema.TupleElement{{Type: dictSchema}, {Type: dictSchema}},
+			}},
+			{Name: "dict", Type: schema.Dict{Key: taggedSchema, Value: dictSchema}},
+		}}},
+	}
+
+	listData := []float64{-1.01, 2.0, 1294.21}
+	structData := map[string]any{"fieldInt16": 100, "fieldFloat32": 100.01, "fieldString": "abc"}
+	tupleData := []any{-5, 300.03, "my data"}
+	namedVariantData := []any{"fieldString", "magotan"}
+	unnamedVariantData := []any{1, 300.03}
+	dictData := [][]any{{"k1", 1}, {"k2", 2}, {"k3", 3}}
+
+	data := []map[string]any{{
+		"id":                uint8(1),
+		"date_str":          "2022-03-10",
+		"datetime_str":      "2022-03-10T01:02:03",
+		"datetime_str2":     "2022-03-10 01:02:03",
+		"datetime_ts":       int64(0),
+		"datetime_ts2":      int64(1646940559),
+		"intlist":           []int64{1, 2, 3},
+		"num_to_str":        int32(100),
+		"decimal_as_float":  2.3456,
+		"decimal_as_string": "23.45",
+		"decimal_as_bytes":  []byte("67.89"),
+
+		"list":            listData,
+		"struct":          structData,
+		"tuple":           tupleData,
+		"variant_named":   namedVariantData,
+		"variant_unnamed": unnamedVariantData,
+		"dict":            dictData,
+		"tagged":          []any{"fieldInt16", 100},
+
+		"nested1": map[string]any{
+			"list":  []any{[]any{dictData, dictData}},
+			"named": []any{"d2", dictData},
+		},
+
+		"nested2": map[string]any{
+			"unnamed": []any{1, dictData},
+			"dict":    [][]any{{10, dictData}, {11, dictData}},
+		},
+	}}
+
+	return schema, data
+}
+
+func ChSchemaForYtTypesTestData() string {
+	return `
+		id UInt8,
+		date_str Date,
+		datetime_str DateTime,
+		datetime_str2 DateTime,
+		datetime_ts DateTime,
+		datetime_ts2 DateTime,
+		intlist Array(Int64),
+		num_to_str String,
+		decimal_as_float Decimal(10, 7),
+		decimal_as_string Decimal(10, 7),
+		decimal_as_bytes Decimal(10, 7),
+
+		struct String,
+		list String,
+		tuple String,
+		variant_named String,
+		variant_unnamed String,
+		dict String,
+		tagged String,
+
+		nested1 String,
+		nested2 String
+	`
 }
