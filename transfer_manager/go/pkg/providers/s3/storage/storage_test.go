99358198a76f79ff733b5e494cad93d880fe3a58
tserakhau <tserakhau@double.cloud>
2024-08-12 23:21:25 +0300

    TRANSFER-638: Demo dumb perf boost

--- transfer_manager/go/pkg/providers/s3/storage/storage_test.go
+++ transfer_manager/go/pkg/providers/s3/storage/storage_test.go
@@ -2,16 +2,22 @@ package storage
 
 import (
 	"context"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"math"
 	"os"
+	"reflect"
 	"sort"
 	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 
+	"github.com/apache/arrow/go/v13/parquet"
+	"github.com/apache/arrow/go/v13/parquet/file"
+	"github.com/apache/arrow/go/v13/parquet/schema"
 	"github.com/doublecloud/tross/library/go/core/metrics/solomon"
 	"github.com/doublecloud/tross/library/go/test/canon"
 	"github.com/doublecloud/tross/transfer_manager/go/internal/logger"
@@ -21,6 +27,209 @@ import (
 	"github.com/doublecloud/tross/transfer_manager/go/pkg/providers/s3"
 	"github.com/stretchr/testify/require"
 )
+
+func TestArrow(t *testing.T) {
+	rdr, err := file.OpenParquetFile("./yellow_taxi/demo.parquet", true)
+	require.NoError(t, err)
+	require.NotNil(t, rdr)
+
+	selectedColumns := []int{}
+	fileMetadata := rdr.MetaData()
+	for i := 0; i < fileMetadata.Schema.NumColumns(); i++ {
+		selectedColumns = append(selectedColumns, i)
+	}
+	fileMetadata.Size()
+	blockSize := 10 * 1024
+
+	st := time.Now()
+	cis := make([]abstract.ChangeItem, 0, blockSize)
+	for r := 0; r < rdr.NumRowGroups(); r++ {
+		rgr := rdr.RowGroup(r)
+		logger.Log.Infof("row group size: %v", rgr.NumRows())
+		scanners := make([]*Dumper, len(selectedColumns))
+		fields := make([]string, len(selectedColumns))
+		for idx, c := range selectedColumns {
+			col, err := rgr.Column(c)
+			require.NoError(t, err)
+			scanners[idx] = createDumper(col)
+			fields[idx] = col.Descriptor().Path()
+		}
+
+	GROUP:
+		for {
+			var ci abstract.ChangeItem
+			ci.Kind = abstract.InsertKind
+			ci.ColumnValues = make([]interface{}, len(fields))
+			ci.ColumnNames = fields
+			ci.Table = "demo"
+			for idx, s := range scanners {
+				if val, ok := s.Next(); ok {
+					if val == nil {
+						continue
+					}
+					ci.ColumnValues[idx] = s.FormatValue(val, 0)
+				} else {
+					break GROUP
+				}
+			}
+			cis = append(cis, ci)
+			if len(cis) == blockSize {
+				logger.Log.Infof("read %v rows", len(cis))
+				cis = make([]abstract.ChangeItem, 0, blockSize)
+			}
+		}
+		if len(cis) > 0 {
+			logger.Log.Infof("group read done, left %v rows", len(cis))
+			cis = make([]abstract.ChangeItem, 0, blockSize)
+		}
+	}
+	logger.Log.Infof("dummy load in: %v", time.Since(st))
+}
+
+const defaultBatchSize = 128
+
+type Dumper struct {
+	reader         file.ColumnChunkReader
+	batchSize      int64
+	valueOffset    int
+	valuesBuffered int
+
+	levelOffset    int64
+	levelsBuffered int64
+	defLevels      []int16
+	repLevels      []int16
+
+	valueBuffer interface{}
+}
+
+func createDumper(reader file.ColumnChunkReader) *Dumper {
+	batchSize := defaultBatchSize
+
+	var valueBuffer interface{}
+	switch reader.(type) {
+	case *file.BooleanColumnChunkReader:
+		valueBuffer = make([]bool, batchSize)
+	case *file.Int32ColumnChunkReader:
+		valueBuffer = make([]int32, batchSize)
+	case *file.Int64ColumnChunkReader:
+		valueBuffer = make([]int64, batchSize)
+	case *file.Float32ColumnChunkReader:
+		valueBuffer = make([]float32, batchSize)
+	case *file.Float64ColumnChunkReader:
+		valueBuffer = make([]float64, batchSize)
+	case *file.Int96ColumnChunkReader:
+		valueBuffer = make([]parquet.Int96, batchSize)
+	case *file.ByteArrayColumnChunkReader:
+		valueBuffer = make([]parquet.ByteArray, batchSize)
+	case *file.FixedLenByteArrayColumnChunkReader:
+		valueBuffer = make([]parquet.FixedLenByteArray, batchSize)
+	}
+
+	return &Dumper{
+		reader:      reader,
+		batchSize:   int64(batchSize),
+		defLevels:   make([]int16, batchSize),
+		repLevels:   make([]int16, batchSize),
+		valueBuffer: valueBuffer,
+	}
+}
+
+func (dump *Dumper) readNextBatch() {
+	switch reader := dump.reader.(type) {
+	case *file.BooleanColumnChunkReader:
+		values := dump.valueBuffer.([]bool)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.Int32ColumnChunkReader:
+		values := dump.valueBuffer.([]int32)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.Int64ColumnChunkReader:
+		values := dump.valueBuffer.([]int64)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.Float32ColumnChunkReader:
+		values := dump.valueBuffer.([]float32)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.Float64ColumnChunkReader:
+		values := dump.valueBuffer.([]float64)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.Int96ColumnChunkReader:
+		values := dump.valueBuffer.([]parquet.Int96)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.ByteArrayColumnChunkReader:
+		values := dump.valueBuffer.([]parquet.ByteArray)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	case *file.FixedLenByteArrayColumnChunkReader:
+		values := dump.valueBuffer.([]parquet.FixedLenByteArray)
+		dump.levelsBuffered, dump.valuesBuffered, _ = reader.ReadBatch(dump.batchSize, values, dump.defLevels, dump.repLevels)
+	}
+
+	dump.valueOffset = 0
+	dump.levelOffset = 0
+}
+
+func (dump *Dumper) hasNext() bool {
+	return dump.levelOffset < dump.levelsBuffered || dump.reader.HasNext()
+}
+
+func (dump *Dumper) Next() (interface{}, bool) {
+	if dump.levelOffset == dump.levelsBuffered {
+		if !dump.hasNext() {
+			return nil, false
+		}
+		dump.readNextBatch()
+		if dump.levelsBuffered == 0 {
+			return nil, false
+		}
+	}
+
+	defLevel := dump.defLevels[int(dump.levelOffset)]
+	// repLevel := dump.repLevels[int(dump.levelOffset)]
+	dump.levelOffset++
+
+	if defLevel < dump.reader.Descriptor().MaxDefinitionLevel() {
+		return nil, true
+	}
+
+	vb := reflect.ValueOf(dump.valueBuffer)
+	v := vb.Index(dump.valueOffset).Interface()
+	dump.valueOffset++
+
+	return v, true
+}
+
+const microSecondsPerDay = 24 * 3600e6
+
+func (dump *Dumper) FormatValue(val interface{}, width int) string {
+	fmtstring := fmt.Sprintf("-%d", width)
+	switch val := val.(type) {
+	case nil:
+		return fmt.Sprintf("%"+fmtstring+"s", "NULL")
+	case bool:
+		return fmt.Sprintf("%"+fmtstring+"t", val)
+	case int32:
+		return fmt.Sprintf("%"+fmtstring+"d", val)
+	case int64:
+		return fmt.Sprintf("%"+fmtstring+"d", val)
+	case float32:
+		return fmt.Sprintf("%"+fmtstring+"f", val)
+	case float64:
+		return fmt.Sprintf("%"+fmtstring+"f", val)
+	case parquet.Int96:
+		usec := int64(binary.LittleEndian.Uint64(val[:8])/1000) +
+			(int64(binary.LittleEndian.Uint32(val[8:]))-2440588)*microSecondsPerDay
+		t := time.Unix(usec/1e6, (usec%1e6)*1e3).UTC()
+		return fmt.Sprintf("%"+fmtstring+"s", t)
+
+	case parquet.ByteArray:
+		if dump.reader.Descriptor().ConvertedType() == schema.ConvertedTypes.UTF8 {
+			return fmt.Sprintf("%"+fmtstring+"s", string(val))
+		}
+		return fmt.Sprintf("% "+fmtstring+"X", val)
+	case parquet.FixedLenByteArray:
+		return fmt.Sprintf("% "+fmtstring+"X", val)
+	default:
+		return fmt.Sprintf("%"+fmtstring+"s", fmt.Sprintf("%v", val))
+	}
+}
 
 func TestCanonParquet(t *testing.T) {
 	testCasePath := "yellow_taxi"
