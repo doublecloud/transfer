de4895dabf30db87860da0ede8ce5a4b91479d43
booec <booec@yandex-team.com>
2024-10-07 14:13:52 +0300

    Disable ydb2ydb OLAP test commit_hash:b9b80e2c897a9522ebdfecb4ea5ea3e77b34255c

--- tests/e2e/ydb2ydb/debezium/snapshot_serde_via_debezium_embedded_olap/check_db_test.go
+++ tests/e2e/ydb2ydb/debezium/snapshot_serde_via_debezium_embedded_olap/check_db_test.go
@@ -26,6 +26,8 @@ var pathOut = "dectest/timmyb32r-test-out"
 var sourceChangeItem abstract.ChangeItem
 
 func TestSnapshotSerDeViaDebeziumEmbeddedOLAP(t *testing.T) {
+	t.SkipNow() // https://st.yandex-team.ru/TM-7986
+
 	src := &ydb.YdbSource{
 		Token:              server.SecretString(os.Getenv("YDB_TOKEN")),
 		Database:           helpers.GetEnvOfFail(t, "YDB_DATABASE"),
