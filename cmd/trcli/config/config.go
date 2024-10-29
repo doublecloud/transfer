943fa00e8b2053517066dec0f2421f4ee03cfbb7
tserakhau <laskoviymishka@gmail.com>
2024-10-29 11:45:09 +0300

    Add env-var substitution for params
    Add ENV-vars for src and dst params, user now may specify:
    
    export FOO=secret
    
    transfer.yaml:
    
    ```
    dst:
      type: ch
      params: |
        {"Password": "${FOO}"}
    ```
    
    After load this transfer yaml would be:
    
    ```
    dst:
      type: ch
      params: |
        {"Password": "secret"}
    ```
    
    resolves: #67
    
    ---
    
    Pull Request resolved: https://github.com/doublecloud/transfer/pull/73
    
    Co-authored-by: tserakhau <tserakhau@double.cloud>
    commit_hash:4f85798cbdc4bc55a45fc090f2c72ac55da22afd

--- cmd/trcli/config/config.go
+++ cmd/trcli/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/doublecloud/transfer/library/go/core/xerrors"
 	"github.com/doublecloud/transfer/pkg/abstract"
@@ -59,6 +61,11 @@ func ParseTransferYaml(rawData []byte) (*TransferYamlView, error) {
 	var transfer TransferYamlView
 	if err := yaml.Unmarshal(rawData, &transfer); err != nil {
 		return nil, err
+	}
+	for _, v := range os.Environ() {
+		pair := strings.SplitN(v, "=", 2)
+		transfer.Src.Params = strings.ReplaceAll(transfer.Src.Params, fmt.Sprintf("${%v}", pair[0]), pair[1])
+		transfer.Dst.Params = strings.ReplaceAll(transfer.Dst.Params, fmt.Sprintf("${%v}", pair[0]), pair[1])
 	}
 	return &transfer, nil
 }
---
+++ cmd/trcli/config/config_test.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseTransferYaml_WithEnvSubstitution(t *testing.T) {
+	require.NoError(t, os.Setenv("FOO", "secret1"))
+	require.NoError(t, os.Setenv("BAR", "secret2"))
+	defer os.Unsetenv("FOO")
+	defer os.Unsetenv("BAR")
+
+	transfer, err := ParseTransferYaml([]byte(`
+src:
+  type: src_type
+  params: |
+    {"Password": "${FOO}"}
+dst:
+  type: dst_type
+  params: |
+    {"Password": "${BAR}"}
+`))
+	require.NoError(t, err)
+
+	assert.Equal(t, "{\"Password\": \"secret1\"}\n", transfer.Src.Params)
+	assert.Equal(t, "{\"Password\": \"secret2\"}\n", transfer.Dst.Params)
+}
