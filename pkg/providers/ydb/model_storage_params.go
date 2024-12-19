580a2ded725f02f3c72d1fd67e4211670c92d942
tserakhau <tserakhau@double.cloud>
2024-12-19 15:30:39 +0300

    Support for OAuth configs in YDB
    Closes https://github.com/doublecloud/transfer/issues/142
    commit_hash:be1f5299a8abc1dce6cac02b4a9110b8adf0b91e

--- pkg/providers/ydb/model_storage_params.go
+++ pkg/providers/ydb/model_storage_params.go
@@ -1,6 +1,9 @@
 package ydb
 
-import "github.com/doublecloud/transfer/pkg/abstract/model"
+import (
+	"github.com/doublecloud/transfer/pkg/abstract/model"
+	v3credential "github.com/ydb-platform/ydb-go-sdk/v3/credentials"
+)
 
 type YdbStorageParams struct {
 	Database           string
@@ -15,6 +18,7 @@ type YdbStorageParams struct {
 	UserdataAuth     bool
 	SAKeyContent     string
 	TokenServiceURL  string
+	OAuth2Config     *v3credential.OAuth2Config
 
 	RootCAFiles []string
 	TLSEnabled  bool
--- pkg/providers/ydb/storage.go
+++ pkg/providers/ydb/storage.go
@@ -56,6 +56,7 @@ func NewStorage(cfg *YdbStorageParams) (*Storage, error) {
 			TokenServiceURL: cfg.TokenServiceURL,
 		},
 		cfg.ServiceAccountID,
+		cfg.OAuth2Config,
 		logger.Log,
 	)
 	if err != nil {
